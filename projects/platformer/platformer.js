7392b7aee28cfe24550f81b01d08d67fbf2e52b1
Susannah Donoghue <loopycandy06@gmail.com>
2023-09-18 16:58:46 +0000

    <aesthetic reVAMP>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -57,9 +57,9 @@ $(function () {
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-    createCollectable('database',580,100,1,1)
-    createCollectable('diamond',1350,270,1,1)
-    createCollectable('max',380,400,1,1)
+    createCollectable('blood',580,100,1,1)
+    createCollectable('blood',1350,270,1,1)
+    createCollectable('blood',380,400,1,1)
 
 
     // TODO 3
@@ -67,9 +67,9 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-    createCannon('left',300,3000,25,25)
-    createCannon('bottom',750,2000,25,25)
-    createCannon('right',300,5000,25,25)
+    createCannon('left',300,3000,75,75)
+    createCannon('bottom',750,2000,75,75)
+    createCannon('right',300,5000,75,75)
 
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
