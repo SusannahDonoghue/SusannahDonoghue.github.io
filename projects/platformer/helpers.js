c112f11b99babbbbbbf93ec07c640f2d76ec2fa8
Susannah Donoghue <loopycandy06@gmail.com>
2023-10-02 17:06:47 +0000

    add bouncing box

--- projects/platformer/helpers.js
+++ projects/platformer/helpers.js
@@ -358,14 +358,14 @@ function projectileCollision() {
 }
 
 function deathOfPlayer() {
-  ctx.fillStyle = "grey";
+  ctx.fillStyle = "black";
   ctx.fillRect(
     canvas.width / 4,
     canvas.height / 6,
     canvas.width / 2,
     canvas.height / 2
   );
-  ctx.fillStyle = "black";
+  ctx.fillStyle = "red";
   ctx.font = "800% serif";
   ctx.fillText(
     "You are dead",
